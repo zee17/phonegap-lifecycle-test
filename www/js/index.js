08ec2928f7ea84487a3b9f07670b403d10faebe6
zee17 <sohz1_15@uni.worc.ac.uk>
2016-10-18 11:20:22 +0100

    1
    1

--- www/js/index.js
+++ www/js/index.js
@@ -10,11 +10,21 @@ var key5 = "Game";
 var value5 = "Loss";
 
     function onLoad() {
-        window.alert(window.localStorage.setItem( key1, value1 ));
-        window.alert(window.localStorage.setItem( key2, value2));
-        window.alert(window.localStorage.setItem( key3, value3 ));
-        window.alert(window.localStorage.setItem( key4, value4 ));
-        window.alert(window.localStorage.setItem( key5, value5 ));
+        
+        window.localStorage.setItem( key1, value1 )
+        window.localStorage.setItem( key2, value2 )
+        window.localStorage.setItem( key3, value3 )
+        window.localStorage.setItem( key4, value4 )
+        window.localStorage.setItem( key5, value5 )
+        
+        
+        
+        
+        window.alert(window.localStorage.getItem( key1));
+        window.alert(window.localStorage.getItem( key2));
+        window.alert(window.localStorage.getItem( key3));
+        window.alert(window.localStorage.getItem( key4));
+        window.alert(window.localStorage.getItem( key5));
     }
     
 	
