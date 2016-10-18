6244f27156ebf28bc1239fd31c5cb9eaf5ee4c8a
zee17 <sohz1_15@uni.worc.ac.uk>
2016-10-18 10:38:50 +0100

    2
    3

--- www/js/index.js
+++ www/js/index.js
@@ -1,16 +1,21 @@
-var key = "pen";
+var key1 = "pen";
 var value = "blue";
-var key = "Money";
+var key2 = "Money";
 var value = "Wallet";
-var key = "Drive";
+var key3 = "Drive";
 var value = "Car";
-var key = "Win";
+var key4 = "Win";
 var value = "Play";
-var key = "Game";
+var key5 = "Game";
 var value = "Loss";
+var value = window.localStorage.getItem(key);
 
     function onLoad() {
         window.localStorage.setItem( key, value );
+    }
+    
+    function Display(){
+        alert("hello my friends");
     }
 	
 	
